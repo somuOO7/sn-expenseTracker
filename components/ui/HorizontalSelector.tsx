883760c682ba6620ba7255ee556c8b13520a6df0
Soumendu Nayak <guitar.soumendu@gmail.com>
2026-01-07 22:52:15 +0530

    Add button added in Horizontal Selector

--- components/ui/HorizontalSelector.tsx
+++ components/ui/HorizontalSelector.tsx
@@ -10,10 +10,19 @@ interface HorizontalSelectorProps {
 
 const HorizontalSelector = (props: HorizontalSelectorProps) => {
   return (
-    <View>
-      <Label variant="semibold" style={{ color: Color.secondary }}>
-        {props.title}
-      </Label>
+    <View style={{ gap: Size.padding / 2 }}>
+      <View style={styles.titleContainer}>
+        <Label variant="semibold" style={{ color: Color.secondary }}>
+          {props.title}
+        </Label>
+        <TouchableOpacity style={styles.addButton}>
+          <Label style={{ color: Color.white }}>Add</Label>
+          <Image
+            source={Icon.plus}
+            style={{ tintColor: Color.white, width: 16, height: 16 }}
+          />
+        </TouchableOpacity>
+      </View>
 
       {props.data.length > 0 ? (
         <ScrollView
@@ -81,6 +90,19 @@ const HorizontalSelector = (props: HorizontalSelectorProps) => {
 };
 
 const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  addButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Color.secondary,
+    borderRadius: Size.borderRadius,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+  },
   itemContainer: {
     height: 72,
     width: 120,
