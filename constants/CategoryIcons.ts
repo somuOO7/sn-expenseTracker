de71997d18f32db625557e210f0645b1bf084d4e
Soumendu Nayak <guitar.soumendu@gmail.com>
2026-01-08 14:05:21 +0530

    Add more expense category

--- constants/CategoryIcons.ts
+++ constants/CategoryIcons.ts
@@ -1,16 +1,15 @@
+import Icon from "./Icons";
+
 const CategoryIcons = [
-  {
-    id: "catIcon_1",
-    icon: require("@/assets/icons/home-outline.svg"),
-  },
-  {
-    id: "catIcon_2",
-    icon: require("@/assets/icons/home-outline.svg"),
-  },
-  {
-    id: "catIcon_3",
-    icon: require("@/assets/icons/home-outline.svg"),
-  },
+  { id: "catIcon_1", icon: Icon.homeOutline },
+  { id: "catIcon_2", icon: Icon.car },
+  { id: "catIcon_3", icon: Icon.dress },
+  { id: "catIcon_4", icon: Icon.food },
+  { id: "catIcon_5", icon: Icon.foodDelivery },
+  { id: "catIcon_6", icon: Icon.gasStation },
+  { id: "catIcon_7", icon: Icon.groceryBag },
+  { id: "catIcon_8", icon: Icon.medical },
+  { id: "catIcon_9", icon: Icon.travel },
 ];
 
 export default CategoryIcons;
